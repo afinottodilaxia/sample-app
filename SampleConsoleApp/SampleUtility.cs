7c27d44b1fc6665d3f43d913f5b7db489a236ee6
Maverick <a.finotto@dilaxia.com>
2025-03-19 17:54:19 +0100

    Added a console app with some example logic

---
+++ SampleConsoleApp/SampleUtility.cs
@@ -0,0 +1,20 @@
+namespace SampleConsoleApp
+{
+    public static class SampleUtility
+    {
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
