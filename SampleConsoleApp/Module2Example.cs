4d623dd2613d962e321d217f743a21a15e2ba4af
Maverick <a.finotto@dilaxia.com>
2025-05-11 15:29:13 +0200

    Added console example

---
+++ SampleConsoleApp/Module2Example.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConsoleApp
+{
+    internal class Module2Example
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Benvenuto nell'applicazione. Scegli un'opzione:");
+            Console.WriteLine("1. Mostra data e ora");
+            Console.WriteLine("2. Saluta l'utente");
+
+            var scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    Console.WriteLine(DateTime.Now);
+                    break;
+                case "2":
+                    Console.Write("Inserisci il tuo nome: ");
+                    string nome = Console.ReadLine();
+                    Console.WriteLine($"Ciao, {nome}!");
+                    break;
+                default:
+                    Console.WriteLine("Opzione non valida.");
+                    break;
+            }
+        }
+    }
+}
