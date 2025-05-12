fb83ed0ec37ec658c9140da4c9489b6006750184
Maverick <a.finotto@dilaxia.com>
2025-05-12 09:02:38 +0200

    Update examples

--- SampleConsoleApp/M3/Methods.cs
+++ SampleConsoleApp/M3/Methods.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SampleConsoleApp
+namespace SampleConsoleApp.M3
 {
-    internal class Module3Methods
+    internal class Methods
     {
         /// <summary>
         /// Calcola il quadrato di un numero intero.
@@ -19,6 +19,23 @@ namespace SampleConsoleApp
             if (numero == null)
                 throw new ArgumentNullException(nameof(numero), "L'input non può essere nullo.");
             return numero.Value * numero.Value;
+        }
+
+
+
+        // Questo metodo prende una lista di stringhe, 
+        // le elabora convertendole in maiuscolo e le unisce in un'unica stringa di output.
+        public string ElaboraLista(List<string> items)
+        {
+            string risultato = "";
+
+            for (int i = 0; i < items.Count; i++)
+            {
+                // Converti ogni elemento in maiuscolo e aggiungi una virgola se non è l'ultimo elemento
+                risultato += items[i].ToUpper() + (i < items.Count - 1 ? ", " : "");
+            }
+
+            return risultato;
         }
     }
 }
